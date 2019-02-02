c9d073e1f1c17859cca92cb743eacab8bc21c200
pachgadehardik <pachgadehardik@gmail.com>
2019-02-03 01:02:55 +0530

    First commit

---
+++ Test.py
@@ -0,0 +1,1 @@
+print("HEllo world!,this is test script automated!!!")
---
+++ test.bat
@@ -0,0 +1,2 @@
+
+C:\Users\hardi\AppData\Local\Programs\Python\Python36\python.exe H:\Hackit\blink-detection\detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.da
